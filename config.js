6962e851c6b6b61c3cc77e6979a9e42ae0a9edf0
Muhammad Faishal Dienul Haq <mhmd.faishal123@gmail.com>
2022-08-27 10:31:29 +0700

    first commit

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const config = {
     db: {
-      /* don't expose password or any sensitive info, done only for demo */
       host: 'localhost',
       user: 'root',
       password: 'Alskdjfhg1q2w3e4r5t',
